ec5b451ba1f688ff1c5e70d3e729d98d8068a317
Gionata Boccalini <gionata.boccalini@gmail.com>
2016-12-18 18:16:56 +0100

    Reorganization of cores and library: changed projects includes and link folder. Removed library since they will move to a different repo.

--- DisplayGroup/Doc/html/search/classes_67.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['groupid',['GroupId',['../structDisplayGroup_1_1GroupId.html',1,'DisplayGroup']]]
-];
--- DisplayGroup/Doc/html/search/functions_70.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['printgroups',['printGroups',['../classDisplayGroup_1_1DisplayManager.html#a357634a070e311b24c6c01c46c92dfad',1,'DisplayGroup::DisplayManager']]]
-];
